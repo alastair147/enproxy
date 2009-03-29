8b67271411a6f5d3e24a61c5e9a06efc3d59ae60
com.chinaos.snaill <com.chinaos.snaill@4f42b9f8-1c10-11de-9433-ad2e3fad96c5>
2009-03-29 12:19:48 +0000

    实现Http Header转换，及代理不重定向的网页

--- trunk/enproxy.py
+++ trunk/enproxy.py
@@ -2,83 +2,55 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import urlfetch
 
 class MainPage(webapp.RequestHandler):
+  DisallowedHeaders = [ 'Content-Length', 'Host', 'Referer', 'User-Agent',
+                        'Vary', 'Via', 'X-Forwarded-For']
+
+  InitHeaders = ['Connection', 'Accept', 'Accept-Charset', 'Accept-Encoding'
+                 'Accept-Language']
+  
+  def error2(self, msg) : 
+    self.redirect('/index.py')
+    
+  def proxy(self, url, initHeader):
+    # parse url
+    if 0 > url.find('://'):
+      url = 'http://' + url
+
+    # fill header
+    headers = {}
+    if initHeader == True :
+      for k, v in self.request.headers.iteritems():
+        if k in self.InitHeaders:
+          headers[k] = v
+    else :
+      for k, v in self.request.headers.iteritems():
+        if k in self.DisallowedHeaders:
+          continue
+        headers[k] = v
+      
+    # fetch url
+    resp = urlfetch.fetch( url, None, urlfetch.GET, headers, False, False );
+
+    # filter urls
+    # response
+    for k, v in resp.headers.iteritems():
+        self.response.headers[k] = v
+    content = resp.content
+    self.response.out.write(content)
+
   def get(self):
-    self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-    self.response.out.write( \
-'''
-<!DOCTYPE html [ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" ]>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-    <script language="javascript">
-
-        function p_submit() {
-            var scheme = '';
-            if (document.main.scheme[0].checked)
-                scheme = document.main.scheme[0].value;
-            else
-                scheme = document.main.scheme[1].value;
-
-            document.p.action = scheme + '://' + 'enproxy.appspot.com/' + urlEncode( document.main.q.value );
-            document.p.submit();
-            return false;
-        }
-    </script>
-</head>
-<body>
-    <table border="0" align="center" width="480px">
-        <tr class="">
-            <td align="center" valign="middle">
-				<br/>
-				<br/>
-				<br/>
-				<br/>
-                <img src="images/logo.gif" />
-                <div style="padding-left: 20px; padding-top: 20px; padding-bottom: 20px">
-                    <div>
-                        <br />
-							<form method="get" accept-charset="utf-8" name="main" onsubmit="return p_submit()">
-								<input type="text" name="q" size="40" />
-								<input type="submit" value=" Go " />
-						    </form>
-                        <label>
-                            <br />
-                            <input type="radio" name="scheme" value="http" checked="checked" />
-                        </label>
-                        HTTP
-                        <label>
-                            <input type="radio" name="scheme" value="https" />
-                        </label>
-                        HTTPS</div>
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td align="left" valign="top" style="padding-left: 50px;">
-                <a href="http://enproxy.appspot.com">enProxy</a> is a free/fast web proxy base on
-                Google App Engine.
-                <br />
-                <br />
-                Why use enProxy ?
-                <ul>
-                    <li><span style="color: #008000">Keeps your IP address (and your identity) hidden.</span></li>
-                    <li><span style="color: #008000">To any place you want to go.</span></li>
-                    <li><span style="color: #008000">It's free, fast, stable.</span></li>
-                    <li><span style="color: #008000"><a href="http://enproxy.appspot.com">Open source</a>.<br />
-                    </span></li>
-                </ul>
-            </td>
-        </tr>
-    </table>
-</body>
-</html>
-''')
-
+    url = self.request.get("url")
+    self.proxy( url, False )
+        
+  def post(self):
+    url = self.request.get("url")
+    self.proxy( url, True )
+            
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/enproxy.py', MainPage)],
                                      debug=True)
 
 def main():
